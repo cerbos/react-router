a3ae11a177488c4789f94201ca64509124e32789
Andrew Haines <haines@cerbos.dev>
2025-05-12 10:20:54 +0100

    Revert "disable fog of war"
    This reverts commit 5c302cbddbc6a03e3c7baca8b12f79b9eef66d65.

--- packages/react-router/lib/dom/ssr/fog-of-war.ts
+++ packages/react-router/lib/dom/ssr/fog-of-war.ts
@@ -27,7 +27,7 @@ const discoveredPaths = new Set<string>();
 const URL_LIMIT = 7680;
 
 export function isFogOfWarEnabled(ssr: boolean) {
-  return false;
+  return ssr === true;
 }
 
 export function getPartialManifest(
